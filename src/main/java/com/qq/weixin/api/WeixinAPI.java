ebaf109c1e514abea1302e8ceae5fd558a42034b
your name <your email>
2020-11-15 22:13:55 +0800

    .

--- src/main/java/com/qq/weixin/api/WeixinAPI.java
+++ src/main/java/com/qq/weixin/api/WeixinAPI.java
@@ -5,47 +5,47 @@ import com.qq.weixin.api.entity.*;
 
 public interface  WeixinAPI {
 
-    public default String _signBody(String sig_method, String session_key, String body) throws Exception {
+    public default String _signBody(String wx_sig_method, String wx_session_key, String wx_body) throws Exception {
         SIGN.Method method;
-        switch (sig_method) {
+        switch (wx_sig_method) {
             case "hmac_sha256":
                 method = SIGN.Method.HMACSHA256;
                 break;
             default:
-                throw new Exception(sig_method);
+                throw new Exception(wx_sig_method);
         }
-        return new SIGN(method).sign(session_key, body);
+        return new SIGN(method).sign(wx_session_key, wx_body);
     }
-    public default String _signRaw(String rawData,String session_key) throws Exception {
-        return new SIGN(SIGN.Method.SHA1).sign( rawData+session_key);
+    public default String _signRaw(String wx_rawData,String wx_session_key) throws Exception {
+        return new SIGN(SIGN.Method.SHA1).sign( wx_rawData+wx_session_key);
     }
-    cgi_bin__token_response cgi_bin__token(String grant_type, String appid, String secret) throws WeixinError;
+    cgi_bin__token_response cgi_bin__token(String wx_grant_type, String wx_appid, String wx_secret) throws WeixinError;
 
-    WeixinResponse wxa__checksession(String access_token, String openid, String signature, String sig_method, String body);
+    WeixinResponse wxa__checksession(String wx_access_token, String wx_openid, String wx_signature, String wx_sig_method, String wx_body);
 
-    snc__jscode2session_response snc__jscode2session(String appid, String secret, String js_code, String grant_type);
+    snc__jscode2session_response snc__jscode2session(String wx_appid, String wx_secret, String wx_js_code, String wx_grant_type);
 
-    WeixinResponse wxa__img_sec_check(String access_token, wxa__img_sec_check_body body);
+    WeixinResponse wxa__img_sec_check(String wx_access_token, byte[] wx_body);
 
-    wxa__media_check_async_response wxa__media_check_async(String access_token, wxa__media_check_async_body body);
+    wxa__media_check_async_response wxa__media_check_async(String wx_access_token, wxa__media_check_async_body wx_body);
 
-    WeixinResponse wxa__msg_sec_check(String access_token, wxa__msg_sec_check_body body);
+    WeixinResponse wxa__msg_sec_check(String wx_access_token, wxa__msg_sec_check_body wx_body);
 
-    WeixinResponse wxa__remove_user_storage(String access_token, String openid, String signature, String sig_method, wxa__remove_user_storage_body body);
+    WeixinResponse wxa__remove_user_storage(String wx_access_token, String wx_openid, String wx_signature, String wx_sig_method, wxa__remove_user_storage_body wx_body);
 
-    WeixinResponse wxa__setuserinteractivedata(String access_token, String openid, String signature, String sig_method, wxa__setuserinteractivedata_body body);
+    WeixinResponse wxa__setuserinteractivedata(String wx_access_token, String wx_openid, String wx_signature, String wx_sig_method, wxa__setuserinteractivedata_body wx_body);
 
-    WeixinResponse wxa__set_user_storage(String access_token, String openid, String signature, String sig_method, wxa__set_user_storage_body body);
+    WeixinResponse wxa__set_user_storage(String wx_access_token, String wx_openid, String wx_signature, String wx_sig_method, wxa__set_user_storage_body wx_body);
 
-    cgi_bin__message__wxopen__activityid__create_response cgi_bin__message__wxopen__activityid__create(String access_token, String unionid);
+    cgi_bin__message__wxopen__activityid__create_response cgi_bin__message__wxopen__activityid__create(String wx_access_token, String unionid);
 
-    WeixinResponse cgi_bin__message__wxopen__updatablemsg__send(String access_token, updatablemsg__send_body body);
+    WeixinResponse cgi_bin__message__wxopen__updatablemsg__send(String wx_access_token, updatablemsg__send_body wx_body);
 
-    byte[] cgi_bin__wxaapp__createwxaqrcode(String access_token, wxaapp__createwxaqrcode_body body) throws WeixinError;
+    byte[] cgi_bin__wxaapp__createwxaqrcode(String wx_access_token, wxaapp__createwxaqrcode_body wx_body) throws WeixinError;
 
-    byte[] wxa__getwxacode(String access_token, wxa__getwxacode_body body)throws WeixinError;
+    byte[] wxa__getwxacode(String wx_access_token, wxa__getwxacode_body wx_body)throws WeixinError;
 
-    byte[] wxa__getwxacodeunlimit(String access_token, wxa__getwxacodeunlimit_body body)throws WeixinError;
+    byte[] wxa__getwxacodeunlimit(String wx_access_token, wxa__getwxacodeunlimit_body wx_body)throws WeixinError;
 
-    WeixinResponse cgi_bin__message__subscribe__send(String access_token, subscribe__send_body body);
+    WeixinResponse cgi_bin__message__subscribe__send(String wx_access_token, subscribe__send_body wx_body);
 }

0aac3eff21d15a4a744dc83700d47551e2d203e8
your name <your email>
2020-11-15 20:56:58 +0800

    .

--- src/main/java/com/qq/weixin/api/WeixinAPI.java
+++ src/main/java/com/qq/weixin/api/WeixinAPI.java
@@ -1,11 +1,24 @@
 package com.qq.weixin.api;
 
+import cn.onekit.thekit.SIGN;
 import com.qq.weixin.api.entity.*;
 
 public interface  WeixinAPI {
 
-    String _signBody(String sig_method, String session_key, String data) throws Exception;
-    String _signRaw(  String rawData,String session_key) throws Exception;
+    public default String _signBody(String sig_method, String session_key, String body) throws Exception {
+        SIGN.Method method;
+        switch (sig_method) {
+            case "hmac_sha256":
+                method = SIGN.Method.HMACSHA256;
+                break;
+            default:
+                throw new Exception(sig_method);
+        }
+        return new SIGN(method).sign(session_key, body);
+    }
+    public default String _signRaw(String rawData,String session_key) throws Exception {
+        return new SIGN(SIGN.Method.SHA1).sign( rawData+session_key);
+    }
     cgi_bin__token_response cgi_bin__token(String grant_type, String appid, String secret) throws WeixinError;
 
     WeixinResponse wxa__checksession(String access_token, String openid, String signature, String sig_method, String body);

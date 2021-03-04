78ef62b051e55b378bbab45923ee0cd331c72fa0
1093357043 <73806828+1093357043@users.noreply.github.com>
2021-03-04 17:56:08 +0800

    .

--- src/main/java/com/qq/weixin/api/request/Code2SessionRequest.java
+++ src/main/java/com/qq/weixin/api/request/Code2SessionRequest.java
@@ -4,10 +4,18 @@ public class Code2SessionRequest {
     private String appid;
     private String secret;
     private String js_code;
+    private Grant_type grant_type;
 
-
-    public enum grant_type{
+    public enum Grant_type{
         authorization_code,
+    }
+
+    public Grant_type getGrant_type() {
+        return grant_type;
+    }
+
+    public void setGrant_type(Grant_type grant_type) {
+        this.grant_type = grant_type;
     }
 
     public String getAppid() {
